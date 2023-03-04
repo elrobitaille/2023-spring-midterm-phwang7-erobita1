f49e5ca91dbdcb8b95cb18c238f7819efbadf990
Edgar Robitaille <erobita1@jh.edu>
2023-03-04 15:59:56 -0500

    Added struct information and added some function declarations

--- puzzle.h
+++ puzzle.h
@@ -5,8 +5,24 @@
 
 typedef struct {
   // ...fields go here...
+  int size; // Size of the 2D image array, number rows or columns 
+  int **grid; // 2D array of integers which are the puzzle tiles
+  int row_index; // The row index of the tile
+  int col_index; // The column index of the tile 
+
 } Puzzle;
 
 // TODO: function declarations
+Puzzle *puzzle_create(int size);
+void puzzle_destroy(Puzzle *p);
+void puzzle_set_tile(Puzzle *p, int col, int row, int value);
+int puzzle_get_tile(const Puzzle *p, int col, int row);
+/* Commands to handle each first letter command case. */
+int handle_C_command(FILE *in, Puzzle **p);
+int handle_T_command(FILE *in, Puzzle *p);
+int handle_I_command(FILE *in, Puzzle *p);
+int handle_W_command(FILE *in, Puzzle *p);
+int handle_S_command(FILE *in, Puzzle *p);
+
 
 #endif // PUZZLE_H
